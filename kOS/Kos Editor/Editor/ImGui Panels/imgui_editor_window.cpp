b3322657461e81f5eb7648539b1f066c58db41a1
ng jaz winn <ngjazwinn@gmail.com>
2025-10-30 21:43:49 +0800

    fix

--- kOS/Kos Editor/Editor/ImGui Panels/imgui_editor_window.cpp
+++ kOS/Kos Editor/Editor/ImGui Panels/imgui_editor_window.cpp
@@ -130,7 +130,10 @@ void gui::ImGuiHandler::DrawRenderScreenWindow(unsigned int windowWidth, unsigne
         // Read just one pixel
         float pixelVal;
         glReadPixels(pixelX, pixelY, 1, 1, GL_ALPHA, GL_FLOAT, &pixelVal);
-        m_clickedEntityId = pixelVal;
+        if (pixelVal >= 0) {
+            m_clickedEntityId = pixelVal;
+        }
+
         //std::cout << "PixelVal is " << pixelVal << '\n';
         //Get texture
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
