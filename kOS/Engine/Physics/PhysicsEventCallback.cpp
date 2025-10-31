82e7ae2c1c624581527b6afd5eb307429bccd37f
yhnothere <rixoth@gmail.com>
2025-10-31 16:43:38 +0800

    Fixed Play Stop Play Crash

--- kOS/Engine/Physics/PhysicsEventCallback.cpp
+++ kOS/Engine/Physics/PhysicsEventCallback.cpp
@@ -6,8 +6,13 @@ namespace physics {
     void PhysicsEventCallback::onContact(const PxContactPairHeader& pairHeader, const PxContactPair* pairs, PxU32 nbPairs) {
         auto pm = PhysicsManager::GetInstance();
 
+        if (pairHeader.flags & (PxContactPairHeaderFlag::eREMOVED_ACTOR_0 | PxContactPairHeaderFlag::eREMOVED_ACTOR_1)) { return; }
+
         for (PxU32 i = 0; i < nbPairs; i++) {
             const PxContactPair& cp = pairs[i];
+
+            if (cp.flags & (PxContactPairFlag::eREMOVED_SHAPE_0 | PxContactPairFlag::eREMOVED_SHAPE_1)) { continue; }
+
             CollisionPair pair{ pairHeader.actors[0], pairHeader.actors[1] };
 
             EntityID entityA = static_cast<EntityID>(reinterpret_cast<uintptr_t>(pairHeader.actors[0]->userData));
@@ -67,7 +72,8 @@ namespace physics {
     }
 
     void PhysicsEventCallback::onTrigger(PxTriggerPair* pairs, PxU32 count) {
-        auto pm = PhysicsManager::GetInstance();
+        auto pm = PhysicsManager::GetInstance();    
+
         for (PxU32 i = 0; i < count; ++i) {
             const PxTriggerPair& tp = pairs[i];
             if (tp.flags & (PxTriggerPairFlag::eREMOVED_SHAPE_TRIGGER | PxTriggerPairFlag::eREMOVED_SHAPE_OTHER)) { continue; }
